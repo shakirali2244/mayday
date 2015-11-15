944461502c6c24c08782a5e596508fa240669b34
Shakir Ali <shakirali2244@yahoo.com>
2015-11-14 23:24:51 -0500

    cleaning: gi;

--- client/js/maps.js
+++ client/js/maps.js
@@ -1,6 +1,5 @@
 var stylesArray = [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
 
-
 function initMap() {
   markers = [];
   // Create a map object and specify the DOM element for display.
@@ -31,11 +30,14 @@ function initMap() {
     socket.emit('chatMessage', { name: 'web map',message: $('#m').val()});
     $('#messages').append($('<li>').text('You:: ' + $('#m').val()));
     $('#m').val('');
-
+    var chat_window = document.getElementById("messages_div");
+    chat_window.scrollTop = chat_window.scrollHeight;
     return false;
   });
   socket.on('chatMessage', function(data){
     $('#messages').append($('<li>').text(data.name + ':: ' +data.message));
+    var chat_window = document.getElementById("messages_div");
+    chat_window.scrollTop = chat_window.scrollHeight;
   });
 }
 
